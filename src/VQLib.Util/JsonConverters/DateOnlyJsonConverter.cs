e9abb2e85680d58eb58cbf5f7be71615299a1956
Vitor Quezada <vitorquezada@gmail.com>
2022-05-12 08:27:59 -0300

    Adição do conversor Json do tipo DateOnly

---
+++ src/VQLib.Util/JsonConverters/DateOnlyJsonConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace VQLib.Util.JsonConverters
+{
+    public class DateOnlyJsonConverter : JsonConverter<DateOnly>
+    {
+        private const string FORMAT = "yyyy-MM-dd";
+
+        public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                return DateOnly.ParseExact(reader.GetString() ?? throw new ArgumentNullException(), FORMAT);
+            }
+
+            throw new JsonException("");
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(FORMAT));
+        }
+    }
+}
