944ee8a2a45f2823c23d7ee3a336302ac715e62d
Vitor Quezada <vitorquezada@gmail.com>
2022-05-19 15:49:29 -0300

    Disponibilização de DbSet para IVQRepository

--- src/VQLib.Relational/Repository/IVQRepository.cs
+++ src/VQLib.Relational/Repository/IVQRepository.cs
@@ -6,6 +6,22 @@ namespace VQLib.Relational.Repository
 {
     public interface IVQRepository<T> : IVQRepositoryWithoutTenant<T> where T : VQBaseEntityTenant
     {
+        Task<bool> AnyUnsafe(IVQSpec<T> spec, CancellationToken cancellationToken = default);
+
+        Task<bool> AnyUnsafe(long id, CancellationToken cancellationToken = default);
+
+        Task<bool> AnyUnsafe(string key, CancellationToken cancellationToken = default);
+
+        Task<bool> AnyUnsafe(CancellationToken cancellationToken = default);
+
+        Task<long> CountUnsafe(CancellationToken cancellationToken = default);
+
+        Task<long> CountUnsafe(IVQSpec<T> spec, CancellationToken cancellationToken = default);
+
+        Task<T> FirstUnsafe(IVQSpec<T> spec, CancellationToken cancellationToken = default);
+
+        Task<TDest> FirstUnsafe<TDest>(IVQSpecTo<T, TDest> spec, CancellationToken cancellationToken = default);
+
         Task<T> GetUnsafe(long id, CancellationToken cancellationToken = default);
 
         Task<T> GetUnsafe(string key, CancellationToken cancellationToken = default);
@@ -21,31 +37,33 @@ namespace VQLib.Relational.Repository
         Task<IEnumerable<TDest>> GetUnsafe<TDest>(IVQSpecTo<T, TDest> spec, CancellationToken cancellationToken = default);
 
         Task<IPagedList<TDest>> GetUnsafe<TDest>(IVQSpecTo<T, TDest> spec, int pageNumber, int pageSize, CancellationToken cancellationToken = default);
-
-        Task<T> FirstUnsafe(IVQSpec<T> spec, CancellationToken cancellationToken = default);
-
-        Task<TDest> FirstUnsafe<TDest>(IVQSpecTo<T, TDest> spec, CancellationToken cancellationToken = default);
-
-        Task<long> CountUnsafe(CancellationToken cancellationToken = default);
-
-        Task<long> CountUnsafe(IVQSpec<T> spec, CancellationToken cancellationToken = default);
-
-        Task<bool> AnyUnsafe(IVQSpec<T> spec, CancellationToken cancellationToken = default);
-
-        Task<bool> AnyUnsafe(long id, CancellationToken cancellationToken = default);
-
-        Task<bool> AnyUnsafe(string key, CancellationToken cancellationToken = default);
-
-        Task<bool> AnyUnsafe(CancellationToken cancellationToken = default);
     }
 
     public interface IVQRepositoryWithoutTenant<T> : IDisposable, IAsyncDisposable where T : VQBaseEntity
     {
+        Task<bool> Any(IVQSpec<T> spec, CancellationToken cancellationToken = default);
+
+        Task<bool> Any(long id, CancellationToken cancellationToken = default);
+
+        Task<bool> Any(string key, CancellationToken cancellationToken = default);
+
+        Task<bool> Any(CancellationToken cancellationToken = default);
+
+        Task<long> Count(CancellationToken cancellationToken = default);
+
+        Task<long> Count(IVQSpec<T> spec, CancellationToken cancellationToken = default);
+
+        Task<int> Delete(long id, CancellationToken cancellationToken = default);
+
+        Task<int> Delete(IEnumerable<long> ids, CancellationToken cancellationToken = default);
+
+        Task<T> First(IVQSpec<T> spec, CancellationToken cancellationToken = default);
+
+        Task<TDest> First<TDest>(IVQSpecTo<T, TDest> spec, CancellationToken cancellationToken = default);
+
         Task<T> Get(long id, CancellationToken cancellationToken = default);
 
         Task<T> Get(string key, CancellationToken cancellationToken = default);
-
-        Task<long> GetIdByKey(string key, CancellationToken cancellationToken = default);
 
         Task<IEnumerable<T>> Get(CancellationToken cancellationToken = default);
 
@@ -59,29 +77,15 @@ namespace VQLib.Relational.Repository
 
         Task<IPagedList<TDest>> Get<TDest>(IVQSpecTo<T, TDest> spec, int pageNumber, int pageSize, CancellationToken cancellationToken = default);
 
-        Task<T> First(IVQSpec<T> spec, CancellationToken cancellationToken = default);
+        IQueryable<T> GetCollection();
 
-        Task<TDest> First<TDest>(IVQSpecTo<T, TDest> spec, CancellationToken cancellationToken = default);
+        IQueryable<T> GetCollectionUnsafe();
 
-        Task<long> Count(CancellationToken cancellationToken = default);
-
-        Task<long> Count(IVQSpec<T> spec, CancellationToken cancellationToken = default);
-
-        Task<bool> Any(IVQSpec<T> spec, CancellationToken cancellationToken = default);
-
-        Task<bool> Any(long id, CancellationToken cancellationToken = default);
-
-        Task<bool> Any(string key, CancellationToken cancellationToken = default);
-
-        Task<bool> Any(CancellationToken cancellationToken = default);
+        Task<long> GetIdByKey(string key, CancellationToken cancellationToken = default);
 
         Task<T> InsertUpdate(T entity, CancellationToken cancellationToken = default);
 
         Task<IEnumerable<T>> InsertUpdate(IList<T> entities, CancellationToken cancellationToken = default);
-
-        Task<int> Delete(long id, CancellationToken cancellationToken = default);
-
-        Task<int> Delete(IEnumerable<long> ids, CancellationToken cancellationToken = default);
 
         Task<int> SaveChanges(CancellationToken cancellationToken = default);
     }
--- src/VQLib.Relational/Repository/VQRepository.cs
+++ src/VQLib.Relational/Repository/VQRepository.cs
@@ -10,54 +10,54 @@ namespace VQLib.Relational.Repository
     {
         private readonly IVQSessionService _sessionService;
 
-        protected override IQueryable<T> _dbSet => base._dbSet.Where(x => x.TenantId == _sessionService.TenantId);
-
         public VQRepository(DbContext dbContext, IVQSessionService sessionService) : base(dbContext)
         {
             _sessionService = sessionService;
         }
+
+        public override IQueryable<T> GetCollection() => base.GetCollection().Where(x => x.TenantId == _sessionService.TenantId);
     }
 
     public class VQRepositoryWithoutTenant<T> : IVQRepositoryWithoutTenant<T>, IDisposable, IAsyncDisposable where T : VQBaseEntity, new()
     {
         private readonly DbContext _dbContext;
 
-        protected virtual IQueryable<T> _dbSet => _dbSetUnsafe;
-
-        protected virtual IQueryable<T> _dbSetUnsafe => _dbContext.Set<T>();
-
         public VQRepositoryWithoutTenant(DbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
+        public virtual IQueryable<T> GetCollection() => GetCollectionUnsafe();
+
+        public virtual IQueryable<T> GetCollectionUnsafe() => _dbContext.Set<T>();
+
         #region Any
 
         public virtual Task<bool> Any(IVQSpec<T> spec, CancellationToken cancellationToken = default) => SetSpecification(spec).AnyAsync(cancellationToken);
 
+        public virtual Task<bool> Any(long id, CancellationToken cancellationToken = default) => GetCollection().AnyAsync(x => x.Id == id, cancellationToken);
+
+        public virtual Task<bool> Any(string key, CancellationToken cancellationToken = default) => GetCollection().AnyAsync(x => x.Key == key, cancellationToken);
+
+        public virtual Task<bool> Any(CancellationToken cancellationToken = default) => GetCollection().AnyAsync(cancellationToken);
+
         public virtual Task<bool> AnyUnsafe(IVQSpec<T> spec, CancellationToken cancellationToken = default) => SetSpecificationUnsafe(spec).AnyAsync(cancellationToken);
 
-        public virtual Task<bool> Any(long id, CancellationToken cancellationToken = default) => _dbSet.AnyAsync(x => x.Id == id, cancellationToken);
+        public virtual Task<bool> AnyUnsafe(long id, CancellationToken cancellationToken = default) => GetCollectionUnsafe().AnyAsync(x => x.Id == id, cancellationToken);
 
-        public virtual Task<bool> AnyUnsafe(long id, CancellationToken cancellationToken = default) => _dbSetUnsafe.AnyAsync(x => x.Id == id, cancellationToken);
+        public virtual Task<bool> AnyUnsafe(string key, CancellationToken cancellationToken = default) => GetCollectionUnsafe().AnyAsync(x => x.Key == key, cancellationToken);
 
-        public virtual Task<bool> Any(string key, CancellationToken cancellationToken = default) => _dbSet.AnyAsync(x => x.Key == key, cancellationToken);
-
-        public virtual Task<bool> AnyUnsafe(string key, CancellationToken cancellationToken = default) => _dbSetUnsafe.AnyAsync(x => x.Key == key, cancellationToken);
-
-        public virtual Task<bool> Any(CancellationToken cancellationToken = default) => _dbSet.AnyAsync(cancellationToken);
-
-        public virtual Task<bool> AnyUnsafe(CancellationToken cancellationToken = default) => _dbSetUnsafe.AnyAsync(cancellationToken);
+        public virtual Task<bool> AnyUnsafe(CancellationToken cancellationToken = default) => GetCollectionUnsafe().AnyAsync(cancellationToken);
 
         #endregion Any
 
         #region Count
 
-        public virtual Task<long> Count(CancellationToken cancellationToken = default) => _dbSet.LongCountAsync(cancellationToken);
-
-        public virtual Task<long> CountUnsafe(CancellationToken cancellationToken = default) => _dbSetUnsafe.LongCountAsync(cancellationToken);
+        public virtual Task<long> Count(CancellationToken cancellationToken = default) => GetCollection().LongCountAsync(cancellationToken);
 
         public virtual Task<long> Count(IVQSpec<T> spec, CancellationToken cancellationToken = default) => SetSpecification(spec).LongCountAsync(cancellationToken);
+
+        public virtual Task<long> CountUnsafe(CancellationToken cancellationToken = default) => GetCollectionUnsafe().LongCountAsync(cancellationToken);
 
         public virtual Task<long> CountUnsafe(IVQSpec<T> spec, CancellationToken cancellationToken = default) => SetSpecificationUnsafe(spec).LongCountAsync(cancellationToken);
 
@@ -67,9 +67,9 @@ namespace VQLib.Relational.Repository
 
         public virtual Task<T> First(IVQSpec<T> spec, CancellationToken cancellationToken = default) => SetSpecification(spec).FirstOrDefaultAsync(cancellationToken);
 
-        public virtual Task<T> FirstUnsafe(IVQSpec<T> spec, CancellationToken cancellationToken = default) => SetSpecificationUnsafe(spec).FirstOrDefaultAsync(cancellationToken);
-
         public virtual Task<TDest> First<TDest>(IVQSpecTo<T, TDest> spec, CancellationToken cancellationToken = default) => SetSpecification(spec).FirstOrDefaultAsync(cancellationToken);
+
+        public virtual Task<T> FirstUnsafe(IVQSpec<T> spec, CancellationToken cancellationToken = default) => SetSpecificationUnsafe(spec).FirstOrDefaultAsync(cancellationToken);
 
         public virtual Task<TDest> FirstUnsafe<TDest>(IVQSpecTo<T, TDest> spec, CancellationToken cancellationToken = default) => SetSpecificationUnsafe(spec).FirstOrDefaultAsync(cancellationToken);
 
@@ -77,37 +77,37 @@ namespace VQLib.Relational.Repository
 
         #region Get
 
-        public virtual Task<T> Get(long id, CancellationToken cancellationToken = default) => _dbSet.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        public virtual Task<T> Get(long id, CancellationToken cancellationToken = default) => GetCollection().FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
 
-        public virtual Task<T> GetUnsafe(long id, CancellationToken cancellationToken = default) => _dbSetUnsafe.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        public virtual Task<T> Get(string key, CancellationToken cancellationToken = default) => GetCollection().FirstOrDefaultAsync(x => x.Key == key, cancellationToken);
 
-        public virtual Task<T> Get(string key, CancellationToken cancellationToken = default) => _dbSet.FirstOrDefaultAsync(x => x.Key == key, cancellationToken);
+        public virtual async Task<IEnumerable<T>> Get(CancellationToken cancellationToken = default) => await GetCollection().ToListAsync(cancellationToken);
 
-        public virtual Task<T> GetUnsafe(string key, CancellationToken cancellationToken = default) => _dbSetUnsafe.FirstOrDefaultAsync(x => x.Key == key, cancellationToken);
-
-        public Task<long> GetIdByKey(string key, CancellationToken cancellationToken = default) => _dbSetUnsafe.Where(x => x.Key == key).Select(x => x.Id).FirstOrDefaultAsync();
-
-        public virtual async Task<IEnumerable<T>> Get(CancellationToken cancellationToken = default) => await _dbSet.ToListAsync(cancellationToken);
-
-        public virtual async Task<IEnumerable<T>> GetUnsafe(CancellationToken cancellationToken = default) => await _dbSetUnsafe.ToListAsync(cancellationToken);
-
-        public virtual Task<IPagedList<T>> Get(int pageNumber, int pageSize, CancellationToken cancellationToken = default) => _dbSet.ToPagedListAsync(pageNumber, pageSize, cancellationToken);
-
-        public virtual Task<IPagedList<T>> GetUnsafe(int pageNumber, int pageSize, CancellationToken cancellationToken = default) => _dbSetUnsafe.ToPagedListAsync(pageNumber, pageSize, cancellationToken);
+        public virtual Task<IPagedList<T>> Get(int pageNumber, int pageSize, CancellationToken cancellationToken = default) => GetCollection().ToPagedListAsync(pageNumber, pageSize, cancellationToken);
 
         public virtual async Task<IEnumerable<T>> Get(IVQSpec<T> spec, CancellationToken cancellationToken = default) => await SetSpecification(spec).ToListAsync(cancellationToken);
 
-        public virtual async Task<IEnumerable<T>> GetUnsafe(IVQSpec<T> spec, CancellationToken cancellationToken = default) => await SetSpecificationUnsafe(spec).ToListAsync(cancellationToken);
-
         public virtual Task<IPagedList<T>> Get(IVQSpec<T> spec, int pageNumber, int pageSize, CancellationToken cancellationToken = default) => SetSpecification(spec).ToPagedListAsync(pageNumber, pageSize, cancellationToken);
-
-        public virtual Task<IPagedList<T>> GetUnsafe(IVQSpec<T> spec, int pageNumber, int pageSize, CancellationToken cancellationToken = default) => SetSpecificationUnsafe(spec).ToPagedListAsync(pageNumber, pageSize, cancellationToken);
 
         public virtual async Task<IEnumerable<TDest>> Get<TDest>(IVQSpecTo<T, TDest> spec, CancellationToken cancellationToken = default) => await SetSpecification(spec).ToListAsync(cancellationToken);
 
-        public virtual async Task<IEnumerable<TDest>> GetUnsafe<TDest>(IVQSpecTo<T, TDest> spec, CancellationToken cancellationToken = default) => await SetSpecificationUnsafe(spec).ToListAsync(cancellationToken);
-
         public virtual Task<IPagedList<TDest>> Get<TDest>(IVQSpecTo<T, TDest> spec, int pageNumber, int pageSize, CancellationToken cancellationToken = default) => SetSpecification(spec).ToPagedListAsync(pageNumber, pageSize, cancellationToken);
+
+        public Task<long> GetIdByKey(string key, CancellationToken cancellationToken = default) => GetCollectionUnsafe().Where(x => x.Key == key).Select(x => x.Id).FirstOrDefaultAsync();
+
+        public virtual Task<T> GetUnsafe(long id, CancellationToken cancellationToken = default) => GetCollectionUnsafe().FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+        public virtual Task<T> GetUnsafe(string key, CancellationToken cancellationToken = default) => GetCollectionUnsafe().FirstOrDefaultAsync(x => x.Key == key, cancellationToken);
+
+        public virtual async Task<IEnumerable<T>> GetUnsafe(CancellationToken cancellationToken = default) => await GetCollectionUnsafe().ToListAsync(cancellationToken);
+
+        public virtual Task<IPagedList<T>> GetUnsafe(int pageNumber, int pageSize, CancellationToken cancellationToken = default) => GetCollectionUnsafe().ToPagedListAsync(pageNumber, pageSize, cancellationToken);
+
+        public virtual async Task<IEnumerable<T>> GetUnsafe(IVQSpec<T> spec, CancellationToken cancellationToken = default) => await SetSpecificationUnsafe(spec).ToListAsync(cancellationToken);
+
+        public virtual Task<IPagedList<T>> GetUnsafe(IVQSpec<T> spec, int pageNumber, int pageSize, CancellationToken cancellationToken = default) => SetSpecificationUnsafe(spec).ToPagedListAsync(pageNumber, pageSize, cancellationToken);
+
+        public virtual async Task<IEnumerable<TDest>> GetUnsafe<TDest>(IVQSpecTo<T, TDest> spec, CancellationToken cancellationToken = default) => await SetSpecificationUnsafe(spec).ToListAsync(cancellationToken);
 
         public virtual Task<IPagedList<TDest>> GetUnsafe<TDest>(IVQSpecTo<T, TDest> spec, int pageNumber, int pageSize, CancellationToken cancellationToken = default) => SetSpecificationUnsafe(spec).ToPagedListAsync(pageNumber, pageSize, cancellationToken);
 
@@ -118,7 +118,7 @@ namespace VQLib.Relational.Repository
         public virtual async Task<T> InsertUpdate(T entity, CancellationToken cancellationToken = default)
         {
             var exist = entity.Id > 0
-                ? await _dbSetUnsafe.IgnoreQueryFilters().AnyAsync(x => x.Id == entity.Id, cancellationToken)
+                ? await GetCollectionUnsafe().IgnoreQueryFilters().AnyAsync(x => x.Id == entity.Id, cancellationToken)
                 : false;
             return await InternalInsertUpdate(entity, true, exist, cancellationToken);
         }
@@ -130,7 +130,7 @@ namespace VQLib.Relational.Repository
             var idList = entities.Where(x => x.Id > 0).Select(x => x.Id).ToHashSet();
             if (idList.Any())
             {
-                var existedIdsSearch = await _dbSetUnsafe.IgnoreQueryFilters()
+                var existedIdsSearch = await GetCollectionUnsafe().IgnoreQueryFilters()
                     .Where(x => idList.Contains(x.Id))
                     .Select(x => x.Id)
                     .ToListAsync(cancellationToken);
@@ -188,14 +188,6 @@ namespace VQLib.Relational.Repository
 
         #region " PRIVATE METHODS "
 
-        private IQueryable<T> SetSpecification(IVQSpec<T> spec) => spec.Order(spec.Specify(_dbSet));
-
-        private IQueryable<TDest> SetSpecification<TDest>(IVQSpecTo<T, TDest> spec) => spec.Order(spec.Specify(_dbSet));
-
-        private IQueryable<T> SetSpecificationUnsafe(IVQSpec<T> spec) => spec.Order(spec.Specify(_dbSetUnsafe));
-
-        private IQueryable<TDest> SetSpecificationUnsafe<TDest>(IVQSpecTo<T, TDest> spec) => spec.Order(spec.Specify(_dbSetUnsafe));
-
         private async Task<T> InternalInsertUpdate(T entity, bool saveChanges, bool exist, CancellationToken cancellationToken = default)
         {
             if (exist)
@@ -212,6 +204,14 @@ namespace VQLib.Relational.Repository
 
             return entity;
         }
+
+        private IQueryable<T> SetSpecification(IVQSpec<T> spec) => spec.Order(spec.Specify(GetCollection()));
+
+        private IQueryable<TDest> SetSpecification<TDest>(IVQSpecTo<T, TDest> spec) => spec.Order(spec.Specify(GetCollection()));
+
+        private IQueryable<T> SetSpecificationUnsafe(IVQSpec<T> spec) => spec.Order(spec.Specify(GetCollectionUnsafe()));
+
+        private IQueryable<TDest> SetSpecificationUnsafe<TDest>(IVQSpecTo<T, TDest> spec) => spec.Order(spec.Specify(GetCollectionUnsafe()));
 
         #endregion " PRIVATE METHODS "
     }
