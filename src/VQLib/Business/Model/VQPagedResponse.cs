ac228caeaf471584cb3098ba1801e1879a4595cc
Vitor Quezada <vitorquezada@gmail.com>
2022-06-27 13:29:19 -0300

    - Ajuste para retorno de VQPagedResponse

--- src/VQLib/Business/Model/VQPagedResponse.cs
+++ src/VQLib/Business/Model/VQPagedResponse.cs
@@ -1,20 +1,48 @@
 ﻿using System.Collections.Generic;
-using Newtonsoft.Json;
+using System.Linq;
 using X.PagedList;
 
 namespace VQLib.Business.Model
 {
-    [JsonObject]
-    public class VQPagedResponse<T> : StaticPagedList<T>
+    public class VQPagedResponse<T>
     {
-        public VQPagedResponse(IEnumerable<T> subset, IPagedList metaData) : base(subset, metaData)
+        private readonly StaticPagedList<T> _pageList;
+
+        public VQPagedResponse(IPagedList<T> pageList)
         {
+            _pageList = new StaticPagedList<T>(pageList.ToList(), pageList);
         }
 
-        public VQPagedResponse(IEnumerable<T> subset, int pageNumber, int pageSize, int totalItemCount) : base(subset, pageNumber, pageSize, totalItemCount)
+        public VQPagedResponse(IEnumerable<T> subset, IPagedList metaData)
         {
+            _pageList = new StaticPagedList<T>(subset, metaData);
         }
 
-        public List<T> Data { get => Subset; }
+        public VQPagedResponse(IEnumerable<T> subset, int pageNumber, int pageSize, int totalItemCount)
+        {
+            _pageList = new StaticPagedList<T>(subset, pageNumber, pageSize, totalItemCount);
+        }
+
+        public List<T> Data { get => _pageList.ToList(); }
+
+        public int PageCount { get => _pageList.PageCount; }
+
+        public int TotalItemCount { get => _pageList.TotalItemCount; }
+
+        public int PageNumber { get => _pageList.PageNumber; }
+
+        public int PageSize { get => _pageList.PageSize; }
+
+        public bool HasPreviousPage { get => _pageList.HasPreviousPage; }
+
+        public bool HasNextPage { get => _pageList.HasNextPage; }
+
+        public bool IsFirstPage { get => _pageList.IsFirstPage; }
+
+        public bool IsLastPage { get => _pageList.IsLastPage; }
+
+        public int FirstItemOnPage { get => _pageList.FirstItemOnPage; }
+
+        public int LastItemOnPage { get => _pageList.LastItemOnPage; }
     }
 }
