0565cb1596decec655383442a232799b00dfc3e5
Vitor Quezada <vitorquezada@gmail.com>
2023-05-12 15:27:31 -0300

    feat: Inclusão de tradução de nome de método para busca com indice de lista

--- src/VQLib.Util/VQExpressionExtension.cs
+++ src/VQLib.Util/VQExpressionExtension.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections;
 using System.Linq.Expressions;
 
 namespace VQLib.Util
@@ -12,17 +12,38 @@ namespace VQLib.Util
 
         public static string GetMemberName(this Expression expression)
         {
+            var call = expression.NodeType == ExpressionType.Call
+                ? (MethodCallExpression)expression
+                : null;
+
             var member = expression.NodeType switch
             {
                 ExpressionType.MemberAccess => (MemberExpression)expression,
                 ExpressionType.Convert => (MemberExpression)((UnaryExpression)expression).Operand,
                 ExpressionType.Parameter => null,
+                ExpressionType.Call => null,
                 _ => throw new NotSupportedException(expression.NodeType.ToString()),
             };
 
-            var name = member == null
-                ? string.Empty
-                : $"{GetMemberName(member.Expression)}.{member.Member.Name}";
+            string name;
+
+            if (call != null
+                && typeof(IEnumerable).IsAssignableFrom(call.Method.DeclaringType)
+                && call.Method.Name == "get_Item")
+            {
+                var field = (MemberExpression)call.Arguments[0];
+                var lambda = Expression.Lambda(field);
+                var fn = lambda.Compile();
+                var index = fn.DynamicInvoke();
+
+                name = $"{GetMemberName(call.Object)}[{index}]";
+            }
+            else
+            {
+                name = member == null
+                    ? string.Empty
+                    : $"{GetMemberName(member.Expression)}.{member.Member.Name}";
+            }
 
             if (name.StartsWith("."))
                 name = name[1..];
