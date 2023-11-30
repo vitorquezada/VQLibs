377f6b261e7932ce32f924df6381681a58c7253c
Vitor Quezada <vitor@vize.solutions>
2023-11-30 17:20:32 -0300

    add Argument null Exception Extensons

---
+++ src/VQLib.Util/Exceptions/VQArgumentNullExceptionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+public static class VQArgumentNullExceptionExtensions
+{
+    public static void ThrowIfNullOrWhiteSpace([NotNull] this string? text, string? paramName = null, string? message = null)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+            throw new ArgumentNullException(paramName, message);
+    }
+
+    public static void ThrowIfNull([NotNull] this object? text, string? paramName = null, string? message = null)
+    {
+        if (text == null)
+            throw new ArgumentNullException(paramName);
+    }
+}
